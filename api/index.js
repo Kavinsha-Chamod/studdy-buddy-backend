3f2575a9589728699c4014b0ec3c14048b617cf1
Kavinsha Chamod <kavinshachamod2001@gmail.com>
2025-04-24 18:07:45 +0530

    Fix import paths for summarize and quiz routes in API index

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const summarizeRoutes = require('./routes/summarizeRoutes');
-const quizRoutes = require('./routes/quizRoutes');
+const summarizeRoutes = require('../routes/summarizeRoutes');
+const quizRoutes = require('../routes/quizRoutes');
 
 const app = express();
 
