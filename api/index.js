6b0d87beb35e9a840840df5179c7e0cfb5995ce4
Kavinsha Chamod <kavinshachamod2001@gmail.com>
2025-04-24 17:52:51 +0530

    Set up API server with Express and CORS; configure Vercel for deployment

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/db');
+
+const summarizeRoutes = require('./routes/summarizeRoutes');
+const quizRoutes = require('./routes/quizRoutes');
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/summarize', summarizeRoutes);
+app.use('/api/quizzes', quizRoutes);
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+module.exports = app;
